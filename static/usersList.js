549ba201a2d1a2ae07ee4188d0c8f22795ef300f
xxxBrandonxxx <108400190+xxxBrandonxxx@users.noreply.github.com>
2022-08-21 12:44:07 +0200

    needs fixing

---
+++ static/usersList.js
@@ -0,0 +1,51 @@
+let  userList = 
+    ["Brandon","Shane","Hansie","Leonardt","Stefan","Gerhardt","Tom","Jason","Sonja","Francois"];
+
+
+
+    function myFunction() {
+        let text;
+        let options = prompt("Click the button What would you like to do?", "1-4");
+        switch(options) {
+          case "1":
+            function getInputFromTextBox() {
+                let selection = document. getElementById("userInput"). value; userList.push([],[],[])};
+            break;
+          case "2":
+            function getInputFromTextBox() {
+                let selection = document. getElementById("userInput"). value; userList.push(getInputFromTextBox)};myArray.pop()
+            break;
+          case "3":
+            function getInputFromTextBox() {
+                let selection = document. getElementById("userInput").innerHTML = userList [0,1,2,3,4,5,6,7,8,9,10]}
+            break;
+          case "4":
+            userList.sort .innerHTML(userList)
+            break;
+          default:
+            text = "I have never heard of that one..";
+        }
+        document.getElementById("demo").innerHTML = text;
+      }
+
+
+      
+
+   
+    // // let option = prompt("What would you like to do?");
+    // let option1 = prompt("What would you like to do?"+ "1) Add three Users");
+    // let option2 = prompt("What would you like to do?"+ "2) Remove User");
+    // let option3 = prompt("What would you like to do?"+ "3) Display all users");
+    // let option4 = prompt("What would you like to do?"+ "4) Sort users");
+
+    // // if (option.toLowerCase() == "scorpio") {
+    // //   alert("Wow! I'm a Scorpio too!");
+    // // }
+    
+    // // // there are many ways to use the prompt feature
+    // // option = window.prompt(); // open the blank prompt window
+    // // option = prompt();       //  open the blank prompt window
+    // // option = window.prompt('Are you feeling lucky'); // open the window with Text "Are you feeling lucky"
+    // // option = window.prompt('Are you feeling lucky', 'sure'); // open the window with Text "Are you feeling lucky" and default value "sure"
+
+
